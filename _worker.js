82edaa6a1091e4838fb26ddb3c3cb756eb8d74b0
Krishnan Ramkumar <krizh1990@gmail.com>
2024-05-31 18:38:33 +0000

    add _worker.js

---
+++ _worker.js
@@ -0,0 +1,11 @@
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const response = await fetch(event.request);
+  // Add custom header here
+  response.headers.set('X-Custom-Header', 'krishnan');
+  return response;
+}
