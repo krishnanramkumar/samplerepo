67ac9b52bd4054a52f06d8d5746a3a588176ff06
Krishnan Ramkumar <krizh1990@gmail.com>
2024-05-31 21:10:16 +0000

    Adding middleware new code

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,6 +3,10 @@ export async function onRequest(context) {
     console.log("Helloooooooo Haiiiii")
     var result =  await context.next();
     console.log(result.headers);
+    result.headers.append(
+      "x-workers-hello",
+      "Hello from Cloudflare Workers"
+    );
     return result;
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
