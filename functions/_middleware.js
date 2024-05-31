77fe6234fc78856279b8ce6af1d3a0f56fa2fa49
Krishnan Ramkumar <krizh1990@gmail.com>
2024-05-31 21:06:26 +0000

    Adding middleware new code

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   try {
     console.log("Helloooooooo Haiiiii")
     var result =  await context.next();
-    console.log(result.response.headers);
+    console.log(result.headers);
     return result;
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
