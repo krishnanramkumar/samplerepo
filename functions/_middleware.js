48cfae0a8b29650ae62a80e9db4f7a77a013016f
Krishnan Ramkumar <krizh1990@gmail.com>
2024-06-06 22:30:10 +0000

    Updating headers with timers

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,11 +2,11 @@ export async function onRequest(context) {
   try {
     var result =  await context.next();
    let before_time_0 = performance.now();
-   let value = await context.env.MY_KV_NAMESPACE.get("onekb");
+   let value_0 = await context.env.MY_KV_NAMESPACE.get("onekb");
    let after_time_0 = performance.now();
    let diff_0 = (after_time_0 - before_time_0).toString();
    let before_time_1 = performance.now();
-   let value = await context.env.MY_KV_NAMESPACE.get("fivetwelvebytes");
+   let value_1 = await context.env.MY_KV_NAMESPACE.get("fivetwelvebytes");
    let after_time_1 = performance.now();
    let diff_1 = (after_time_1 - before_time_1).toString();
    result.headers.append(
