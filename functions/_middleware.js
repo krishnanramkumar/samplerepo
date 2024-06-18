a056b32f81d1a6f9016ebc819e7ebf5d9a7a6839
Krishnan Ramkumar <krizh1990@gmail.com>
2024-06-18 18:15:44 +0000

    Updating the code to cold start 512 bytes

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,20 +5,20 @@ export async function onRequest(context) {
  //  let value_0 = await context.env.MY_KV_NAMESPACE.get("onekb");
   // let after_time_0 = Date.now();
    //let diff_0 = (after_time_0 - before_time_0).toString();
-   //let before_time_1 = Date.now();
-   //let value_1 = await context.env.MY_KV_NAMESPACE.get("fivetwelvebytes");
-   //let after_time_1 = Date.now();
-   //let diff_1 = (after_time_1 - before_time_1).toString();
-   let before_time_2 = Date.now();
-   let value_2 = await context.env.MY_KV_NAMESPACE.get("fourhundredkb");
-   let after_time_2 = Date.now();
-   let diff_2 = (after_time_2 - before_time_2).toString();
+   let before_time_1 = Date.now();
+   let value_1 = await context.env.MY_KV_NAMESPACE.get("fivetwelvebytes");
+   let after_time_1 = Date.now();
+   let diff_1 = (after_time_1 - before_time_1).toString();
+  // let before_time_2 = Date.now();
+   //let value_2 = await context.env.MY_KV_NAMESPACE.get("fourhundredkb");
+   //let after_time_2 = Date.now();
+   //let diff_2 = (after_time_2 - before_time_2).toString();
    //result.headers.append(
     //  "onekb",
      // diff_0
     //);
-    //result.headers.append("fivetwelvebytes", diff_1);
-    result.headers.append("fourhundredkb",diff_2);
+    result.headers.append("fivetwelvebytes", diff_1);
+    //result.headers.append("fourhundredkb",diff_2);
    result.headers.set("cache-control", "s-maxage=604800");
     return result;
   } catch (err) {
