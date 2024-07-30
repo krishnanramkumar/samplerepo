2a0e885bca7f518cb5ad775a7b158094ac701652
Krishnan Ramkumar <krizh1990@gmail.com>
2024-07-30 01:13:52 +0000

    updating middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,48 +1,34 @@
 export async function onRequest(context) {
-  try {
-    var result =  await context.next();
-    //let before_time_0 = Date.now();
-    //let value_0 = await context.env.MY_KV_NAMESPACE.get("onekb");
-    //let after_time_0 = Date.now();
-    //let diff_0 = (after_time_0 - before_time_0).toString();
-   //let before_time_1 = Date.now();
-   //let value_1 = await context.env.MY_KV_NAMESPACE.get("fivetwelvebytes");
-   //let after_time_1 = Date.now();
-   //let diff_1 = (after_time_1 - before_time_1).toString();
-     
-     let before_time_2 = Date.now();
-     //let value_2, = await context.env.MY_KV_NAMESPACE.get("fourhundredkb");
-     let [value_2, fetching_from] = await fetchvaluefromKV(context, "fourhundredkb");
-     let after_time_2 = Date.now();
-     let diff_2 = (after_time_2 - before_time_2).toString();
-   //result.headers.append(
-    //  "onekb",
-     // diff_0
-    //);
-    //result.headers.append("fivetwelvebytes", diff_1);
-    result.headers.append("fourhundredkb",diff_2);
-    result.headers.append("fourhundredkbvalue",value_2.substring(0,100));
-    result.headers.append("fetchingfrom", fetching_from);
-    return result;
-  } catch (err) {
-    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
-  }
+	try {
+
+                var request = context.request()
+                  const keys = []
+
+  // Check for custom headers
+  		for (const [key, value] of request.headers.entries()) {
+    		// Check if the header key starts with 'x-' or 'X-' (common prefix for custom headers)
+    		if (key.startsWith('x-') || key.startsWith('X-')) {
+      			keys.push(value)
+    		}
+  		}
+                
+		var result = await context.next();
+		//var keys = ['fourhundredkb', 'fivetwelvebytes', 'onekb'];
+        for (const item of keys){
+		    let before_time = Date.now();
+                    let value = await context.env.MY_KV_NAMESPACE.get(item);
+		    let after_time = Date.now();
+		    if (value == null)
+                     {
+                        result.headers.append(item, "Error fetching from  KV");
+                      }else {
+                    let diff = (after_time - before_time).toString();	
+		    result.headers.append(item, diff);
+                     }
+        }
+		return result;
+	} catch (err) {
+		return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+	}
 }
 
-let cache = {};
-
-async function fetchvaluefromKV(context, key) {
-// Check if the value is already in memory and not expired
-  if (cache[key] && cache[key].expiry > Date.now()) {
-    return [cache[key].value, "from local cache"];
-  } else {
-    // Fetch value from KV store
-    const value = await context.env.MY_KV_NAMESPACE.get(key);
-    // Store value in memory with a TTL of 30 seconds
-    cache[key] = {
-      value: value,
-      expiry: Date.now() + 30000 // 30 seconds
-    };
-    return [cache[key].value, "from DB"];
-}
-}
